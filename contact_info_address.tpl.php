7cebe98ff57adc8c009a0b15148debd2a7809b7e
Brad Czerniak <ao5357@gmail.com>
2015-01-15 15:56:52 -0500

    Rejiggered the addresses and phones

---
+++ contact_info_address.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * A template for a postal address.
+ */
+?>
+<address class="contact-info-address" itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">
+  <?php if($legal_name): ?>
+  <span itemprop="legalName"><?php print $legal_name; ?></span><br />
+  <?php endif; ?>
+  <?php if($street_address): ?>
+  <span itemprop="streetAddress"><?php print $street_address; ?></span><br />
+  <?php endif; ?>
+  <?php if($locality): ?>
+  <span itemprop="addressLocality"><?php print $locality; ?></span>, 
+  <?php endif; ?>
+  <?php if($region): ?>
+  <span itemprop="addressRegion"><?php print $region; ?></span> 
+  <?php endif; ?>
+  <?php if($postal_code): ?>
+  <span itemprop="postalCode"><?php print $postal_code; ?></span>
+  <?php endif; ?>
+  <?php if($country_name): ?>
+  <span class="element-invisible" itemprop="addressCountry"><?php print $country_name; ?></span>
+  <?php endif; ?>
+</address>
+<?php if($phone): ?>
+  <?php print $phone; ?>
+<?php endif; ?>
+<?php if($fax): ?>
+  <br />
+  <?php print $fax; ?>
+<?php endif; ?>
