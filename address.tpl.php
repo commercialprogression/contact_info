5ea901ee58823e657470e1f7c4a32adad4b6f1a2
Brad Czerniak <ao5357@gmail.com>
2015-04-06 11:44:20 -0400

    Match block, tokens, and schema for street address line 2

--- address.tpl.php
+++ address.tpl.php
@@ -6,25 +6,23 @@
  */
 ?>
 <address class="contact-info-address" itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">
-  <?php if($legal_name): ?>
+  <?php if ($legal_name): ?>
   <span itemprop="legalName"><?php print $legal_name; ?></span><br />
   <?php endif; ?>
-  <?php if($street_address): ?>
-    <span itemprop="streetAddress"><?php print $street_address; ?></span><br />
-  <?php endif; ?>
-  <?php if($street_address_2): ?>
-    <span itemprop="streetAddress"><?php print $street_address_2; ?></span><br />
-  <?php endif; ?>
-  <?php if($locality): ?>
+  <span itemprop="streetAddress">
+  <?php if ($street_address) print $street_address . '<br />'; ?>
+  <?php if ($street_address_2) print $street_address_2 . '<br />'; ?>
+  </span>
+  <?php if ($locality): ?>
   <span itemprop="addressLocality"><?php print $locality; ?></span>, 
   <?php endif; ?>
-  <?php if($region): ?>
+  <?php if ($region): ?>
   <span itemprop="addressRegion"><?php print $region; ?></span> 
   <?php endif; ?>
-  <?php if($postal_code): ?>
+  <?php if ($postal_code): ?>
   <span itemprop="postalCode"><?php print $postal_code; ?></span>
   <?php endif; ?>
-  <?php if($country_name): ?>
+  <?php if ($country_name): ?>
   <span class="element-invisible" itemprop="addressCountry"><?php print $country_name; ?></span>
   <?php endif; ?>
 </address>
