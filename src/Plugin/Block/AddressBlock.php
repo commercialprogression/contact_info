9b2b001011ae852668cf24e21232804bac66ff9a
Hillary Lewandowski <hillary@commercialprogression.com>
2015-12-08 16:46:29 -0500

    Initial D8 port

---
+++ src/Plugin/Block/AddressBlock.php
@@ -0,0 +1,27 @@
+<?php
+namespace Drupal\contact_info\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a block with an address.
+ *
+ * @Block(
+ *   id = "address",
+ *   admin_label = @Translation("Address"),
+ * )
+ */
+class AddressBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Grab contact info data.
+    $contact_info = \Drupal::config('contact_info.setup');
+
+    // Set up variables to send to the theme function.
+    $variables = contact_info_create_address($contact_info);
+
+    return $variables;
+  }
+}
---
+++ src/Plugin/Block/SocialBlock.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\contact_info\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a block with social links.
+ *
+ * @Block(
+ *   id = "social",
+ *   admin_label = @Translation("Social"),
+ * )
+ */
+class SocialBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $contact_info = \Drupal::config('contact_info.setup');
+
+    return contact_info_create_social($contact_info);
+  }
+}
